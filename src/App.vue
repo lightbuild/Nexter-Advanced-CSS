53a0f7af1bbb2575d992805358ecba52b98895a1
lightbuild <renran6303@gmail.com>
2022-09-05 14:23:21 +0800

    完成feature部分的样式

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,48 @@
     </div>
     
     <section class="features">
-      Features
+      <div class="feature">
+        <svg class="feature__icon">
+          <use xlink:href="./assets/images/sprite.svg#icon-star"></use>
+        </svg>
+        <h4 class="heading-4 heading-4--dark">World's best luxury homes</h4>
+        <p class="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit..</p>
+      </div>
+      <div class="feature">
+        <svg class="feature__icon">
+          <use xlink:href="./assets/images/sprite.svg#icon-map"></use>
+        </svg>
+        <h4 class="heading-4 heading-4--dark">Only the best properites </h4>
+        <p class="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit..</p>
+      </div>
+      <div class="feature">
+        <svg class="feature__icon">
+          <use xlink:href="./assets/images/sprite.svg#icon-location-pin"></use>
+        </svg>
+        <h4 class="heading-4 heading-4--dark">All home sin in top location</h4>
+        <p class="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing elit.elit.</p>
+      </div>
+      <div class="feature">
+        <svg class="feature__icon">
+          <use xlink:href="./assets/images/sprite.svg#icon-key"></use>
+        </svg>
+        <h4 class="heading-4 heading-4--dark">New home in one week</h4>
+        <p class="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing eliLorem ipsum dolor sit amet, consectetur adipisicing elit.t.</p>
+      </div>
+      <div class="feature">
+        <svg class="feature__icon">
+          <use xlink:href="./assets/images/sprite.svg#icon-bookmark"></use>
+        </svg>
+        <h4 class="heading-4 heading-4--dark">Top 1% realtors</h4>
+        <p class="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit..</p>
+      </div>
+      <div class="feature">
+        <svg class="feature__icon">
+          <use xlink:href="./assets/images/sprite.svg#icon-aircraft-take-off"></use>
+        </svg>
+        <h4 class="heading-4 heading-4--dark">Secure payments</h4>
+        <p class="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing eliLorem ipsum dolor sit amet, consectetur adipisicing elit.t.</p>
+      </div>
     </section>
     
     <div class="story__pictures">
